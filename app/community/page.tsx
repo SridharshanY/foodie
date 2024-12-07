bf794b0b985d74296b163d40c9cd5f6f084e3c4c
SridharshanY <sridharshany.2001@gmail.com>
2024-12-07 12:51:37 +0530

    made home page

---
+++ app/community/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CommunityPage = () => {
+  return (
+    <div>
+      Community Page
+    </div>
+  )
+}
+
+export default CommunityPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import Link from "next/link";
+
+const Home = () => {
   return (
     <div>
       <h1 className="text-4xl">Hello Next</h1>
+      <p>
+        <Link href='/meals'>Meals</Link>
+        <Link href='/meals/share'>Share Meal</Link>        
+        <Link href='/community'>Community</Link>
+      </p>
     </div>
   );
-}
+};
+
+export default Home;
